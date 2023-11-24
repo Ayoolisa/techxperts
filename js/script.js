9f91f1c881ee522067c151671c9a0fe998881f9a
Ayomide Olisa <lekelisa247@gmail.com>
2023-11-24 22:43:58 +0100

    hot and new updates

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    loop: true,
+    centreSlide: true,
+    fade: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 2,
+        },
+
+        1300: {
+            slidesPerView: 3,
+        },
+    },
+  });
